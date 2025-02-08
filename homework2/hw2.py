89b8ad5b7bf5d7df52d9e3736cb94f3ee64aff52
Sarai Garcia <sarai_garcia@berkeley.edu>
2025-02-07 18:21:09 -0800

    Adding homework 2

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,20 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+#     - The command that would allow me to navigate directly to ~/Desktop would be cd ~/Desktop or cd ../..
 # 2) What does ~/ mean?
+#     - ~/ means to go to home directory.
 # 3) What's the difference between and abolute and a relative path in your terminal?
+#     - Absolute path takes you directly to a location using / while relative path takes you to a place within the current directory using ..
 # 4) What command would bring you back to your home directory?
+#     - You can use cd to bring back to the home directory
 # 5) If you called rm ./ in your current working directory, what would it do? 
+#     - If I called rm ./ it would delete the file after the / within that current working directory.
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+#     - Git add causes to add to directory in it rough draft form preparing it for staging area. Git commit puts it into a local repository and staging area. Git push puts it in a remote repository ready for collaboration.
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+#     - If I were to call "git add" in my current working directory and then "git status" it would tell stage changes and tell me the status of my staging area and which files are in the staging area but not committed yet.
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+#     - An error that I've had is with the different branching so having to put git origin master instead of main.
 # 9) How does cloning a repository differ from pulling from a repository?
+#     - Cloning a repository is like downloading the repository while pulling is like updating the repository.
 # 10) Tell me a fun fact!
+#     - Prices ending in .97 at Costco are the lowest prices for that item.
